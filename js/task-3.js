766e5161e16ee1e0e5d51eab09515e186c3e83c0
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-06-24 15:42:42 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+	let resArray = [];
+	for (const num of numbers) {
+		if (num > value) {
+			resArray.push(num);
+		}
+	}
+	return resArray;
+}
+
+console.log('--- 3rd Task Result ---');
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
